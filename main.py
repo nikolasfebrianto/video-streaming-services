8460f720b2b61fd59e487e447442cc30b613e4d3
Nikolas Febrianto <nikolasfebrianto@gmail.com>
2025-08-24 05:31:01 +0800

    menambahkan file main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 #import library
 from tabulate import tabulate
 
+data = {
+    "Shandy": ["Basic Plan", 12, "shandy-2134"],
+    "Cahya": ["Standard Plan", 24, "cahya-abcd"],
+    "Ana": ["Premium Plan", 5, "ana-2f9g"],
+    "Bagus": ["Basic Plan", 11, "bagus-9f92"]
+}
+
 print("test")
